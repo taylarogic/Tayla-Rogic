7daa9a18e028772d14be1d508f92f709a07eb9db
Tayla Rogic <68985112+taylarogic@users.noreply.github.com>
2021-08-04 20:52:12 +1000

    Update gko.js

--- gko/gko.js
+++ gko/gko.js
@@ -44,7 +44,7 @@ function setup() {
     makeCircle({
       radius: 40,
       increment: -4,
-      string: ':)',
+      string: 'lol',
       color: color('blue'),
       center: {
         x: 80,
